0aef5b8a856528008dc803ed2bd875902abb842e
Pol <pboschmestras@gmail.com>
2026-02-27 20:27:08 -0800

    Setup Payload + Postgres + tests (Vitest/Playwright)

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,8 @@ import { defineConfig, devices } from '@playwright/test'
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import 'dotenv/config'
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.e2e' });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -22,7 +23,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
