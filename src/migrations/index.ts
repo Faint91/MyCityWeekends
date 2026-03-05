74ea107c5ba5292fb6ee6be2815a6d1ee3c3a938
Pol <pboschmestras@gmail.com>
2026-03-04 20:03:44 -0800

    new migration files

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,1 +1,9 @@
-export const migrations = []
+import * as migration_20260305_040259 from './20260305_040259';
+
+export const migrations = [
+  {
+    up: migration_20260305_040259.up,
+    down: migration_20260305_040259.down,
+    name: '20260305_040259'
+  },
+];
